b0c575a67e839afc2b41b86c0d6e48d4d19d5e56
126CChiu <126cchiu@frhsd.com>
2026-01-08 08:28:13 -0500

    day 2

--- server.js
+++ server.js
@@ -9,13 +9,38 @@ app.use(cors({
 app.use(bodyParser.json());
 const port = 3000;
 
-const message = "Hello!";
+const arr= [];
 app.get("/", (req, res) => {
     res.json({
-        data: message
+        data: arr,
     });
 })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
+})
+
+app.post('/users', (req, res) => {
+    const {email, password, id} = req.body;
+    if(!password || !email || !id) {
+        return res.status(400).send('Email, password and id are required');
+    }
+
+    arr.push(req.body);
+    console.log('User created: ', arr);
+    res.status(200).json(arr);
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const {email, password, id} = req.body;
+    const userIndex = arr.findIndex(u => u.id === userId);
+
+    if(userIndex === -1) {
+        return res.status(404).send('User not found');
+    }
+
+    arr[userIndex] = {email, password, id};
+    console.log('User updated:', req.body);
+    res.status(200).send({password});
 })
