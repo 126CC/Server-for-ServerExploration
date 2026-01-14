1280db3b5566af8eef392342276bfa49946f3127
126CChiu <126cchiu@frhsd.com>
2026-01-14 08:19:31 -0500

    commit

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const cors = require('cors') //Allows access on localhost
 const bodyParser = require("body-parser"); //Allows json data in body section
 
 const app = express();
+
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'] // Specify allowed methods
 }));
+
 app.use(bodyParser.json());
 const port = 3000;
 
@@ -14,6 +16,7 @@ let nextUserId = 1
 let users = [
     { id: 0, email: 'JohnDose@gmail.com', pass: '123456' }
 ];
+
 app.get("/:index", (req, res) => {
     res.status(200).json(users[JSON.parse(req.params.index)]);
 })
@@ -26,8 +29,7 @@ app.post("/users", (req, res) => {
     const{email, pass} = req.body;
     if(!pass || !email){
         res.status(400).json({error: 'Please enter a valid email and name'});
-    }
-    else {
+    } else {
         const newUser = {id: nextUserId++, email, pass};
         users.push(newUser);
         console.log('user created', newUser);
@@ -51,8 +53,7 @@ app.delete('/users/:id', (req, res) => {
     const deleteId = parseInt(req.params.id);
     if (deleteId === -1) {
         return  res.status(404).json({ message: 'Item not found' });
-    }
-    else{
+    } else {
         console.log("User deleted at, "+deleteId +" remainingItems" +users);
         users.splice(deleteId, 1);
         res.status(200).json({ message: `Item with ID: ${deleteId} deleted successfully`, remainingItems: users});
